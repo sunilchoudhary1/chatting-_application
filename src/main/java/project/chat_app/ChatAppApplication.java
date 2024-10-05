c765a8f3d344348d33f7ce70348f028a31637acb
Sunil <sunilkumar010@gmail.com>
2024-10-05 11:15:27 +0530

    first commit

--- src/main/java/project/chat_app/ChatAppApplication.java
+++ src/main/java/project/chat_app/ChatAppApplication.java
@@ -2,6 +2,7 @@ package project.chat_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import project.chat_app.client.ClientGUI;
 
 @SpringBootApplication
 public class ChatAppApplication {
